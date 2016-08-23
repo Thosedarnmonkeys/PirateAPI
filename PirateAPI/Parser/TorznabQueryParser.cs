2464d52b69725e0b07db238afea27713c32f1c1e
Thosedarnmonkeys <thosedarnmonkeys@googlemail.com>
2016-08-23 22:05:02 +0100

    Added absolute base of Parser

---
+++ PirateAPI/Parser/TorznabQueryParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirateAPI.Parser
+{
+  public class TorznabQueryParser
+  {
+    public string Parse(string query)
+    {
+
+    }
+  }
+}
---
+++ Tests/Tests/TorznabQueryParserTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using PirateAPI.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Tests
+{
+  [TestFixture]
+  public class TorznabQueryParserTests
+  {
+    [Test]
+    public void TestBasicParse()
+    {
+      string torznabQuery = "http://localhost:8080/api?t=tvsearch&cat=5030,5040&extended=1&offset=0&limit=100";
+      string pirateProxy = "http://fakepirateproxy.com";
+
+      TorznabQueryParser parser = new TorznabQueryParser();
+
+      string parsedRequest = parser.Parse(torznabQuery);
+
+      string correctResponse = "http://fakepirateproxy.com/s/?q=Show+Name&page=0&orderby=99";
+
+      Assert.AreEqual(correctResponse, parsedRequest);
+    }
+  }
+}
