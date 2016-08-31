8a23502da42db31b91a277e040f6137ea6cadd2e
Thosedarnmonkeys <thosedarnmonkeys@googlemail.com>
2016-08-31 14:10:18 +0100

    Started adding HtmlTorrentTableRowParser Tests

--- PirateAPI/Parsers/Torrents/HtmlTorrentTableRowParser.cs
+++ PirateAPI/Parsers/Torrents/HtmlTorrentTableRowParser.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PirateAPI.Logging;
 
 namespace PirateAPI.Parsers.Torrents
 {
-  class HtmlTorrentTableRowParser
+  public class HtmlTorrentTableRowParser
   {
+    #region private fields
+    private ILogger logger;
+    #endregion
+
+    #region constructor
+    public HtmlTorrentTableRowParser(ILogger logger)
+    {
+      if (logger == null)
+        throw new ArgumentException("HtmlTorrentTableRowParser Constructor was passed a null ILogger", nameof(logger));
+
+      this.logger = logger;
+    }
+    #endregion
+
+    #region public methods
+    public Torrent ParseRow(string rowString)
+    {
+      throw new NotImplementedException();
+    }
+    #endregion
   }
 }
--- PirateAPI/Parsers/Torrents/Torrent.cs
+++ PirateAPI/Parsers/Torrents/Torrent.cs
@@ -6,11 +6,47 @@ using System.Threading.Tasks;
 
 namespace PirateAPI.Parsers.Torrents
 {
+  public enum TorrentUploaderStatus { None, Trusted, Vip}
+
   public class Torrent
   {
     public string Title { get; set; }
-    public int Id { get; set; }
     public DateTime PublishDate { get; set; }
     public string Link { get; set; }
+    public TorrentUploaderStatus UploaderStatus { get; set; }
+    public long Size { get; set; }
+    public int Seeds { get; set; }
+    public int Leeches { get; set; }
+
+    public override bool Equals(object obj)
+    {
+      Torrent other = obj as Torrent;
+
+      if (other == null)
+        return false;
+
+      if (other.Title != Title)
+        return false;
+
+      if (other.PublishDate != PublishDate)
+        return false;
+
+      if (other.Link != Link)
+        return false;
+
+      if (other.UploaderStatus != UploaderStatus)
+        return false;
+
+      if (other.Size != Size)
+        return false;
+
+      if (other.Seeds != Seeds)
+        return false;
+
+      if (other.Leeches != Leeches)
+        return false;
+
+      return true;
+    }
   }
 }
