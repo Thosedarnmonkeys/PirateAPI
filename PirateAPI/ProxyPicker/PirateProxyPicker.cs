009054c402cf8dc36b53cec1042d29f16fc244ea
Thosedarnmonkeys <thosedarnmonkeys@googlemail.com>
2016-09-01 12:05:00 +0100

    Renamed file folder and namespace to be consistent with project

---
+++ PirateAPI/ProxyPicker/PirateProxyPicker.cs
@@ -0,0 +1,78 @@
+﻿using PirateAPI.Logging;
+using PirateAPI.ProxyProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirateAPI.ProxyPicker
+{
+  public class PirateProxyPicker
+  {
+    #region private fields
+    private ILogger logger;
+    private List<string> locationPrefs;
+    private List<string> blackListedDomains = new List<string>();
+    #endregion
+
+    #region constructor
+    public PirateProxyPicker(List<string> locationPrefs, ILogger logger)
+    {
+      this.locationPrefs = locationPrefs;
+      this.logger = logger;
+    }
+    #endregion
+
+    #region public methods
+    public Proxy BestProxy(List<Proxy> proxies)
+    {
+      IEnumerable<Proxy> goodProxies = from p in proxies
+                                       where !blackListedDomains.Contains(p.Domain)
+                                       select p;
+
+      if (goodProxies.Count() == 0)
+      {
+        logger.LogError("All available proxies are blacklisted");
+        return null;
+      }
+
+      IEnumerable<Proxy> matchedProxies = null;
+
+      if (locationPrefs != null && locationPrefs.Count != 0)
+      {
+        foreach(string loc in locationPrefs)
+        {
+          if (goodProxies.Any(p => p.Country == loc))
+          {
+            matchedProxies = from p in goodProxies
+                                    where p.Country == loc
+                                    select p;
+            break;
+          }
+        }
+      }
+
+      if (matchedProxies == null)
+        matchedProxies = goodProxies;
+
+      matchedProxies = matchedProxies.OrderBy(p => p.Speed);
+
+      Proxy bestProxy = matchedProxies.FirstOrDefault();
+
+      logger.LogMessage($"Picker determined best proxy was domain {bestProxy.Domain} with speed {bestProxy.Speed} and location {bestProxy.Country}");
+
+      return bestProxy;
+    }
+
+    public void BlacklistDomain(string domain)
+    {
+      if (!blackListedDomains.Contains(domain))
+      {
+        logger.LogMessage($"Domain {domain} was blacklisted");
+        blackListedDomains.Add(domain);
+      }
+    }
+    #endregion
+  }
+}
---
+++ Tests/Tests/PirateProxyPickerTests.cs
@@ -0,0 +1,169 @@
+﻿using NUnit.Framework;
+using PirateAPI.ProxyPicker;
+using PirateAPI.ProxyProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PirateAPITests.Tests.StubClasses;
+
+namespace PirateAPITests.Tests
+{
+  [TestFixture]
+  public class ProxyPickerTests
+  {
+    [Test]
+    public void TestBestProxySpeedFullSpread()
+    {
+      List<string> locationPrefs = new List<string> { "uk", "us", "sd" };
+      Proxy bestProxy = new Proxy { Country = "uk", Domain = "domain", Speed = ProxySpeed.VeryFast };
+
+      List<Proxy> proxies = new List<Proxy>
+      {
+        new Proxy {Country = "uk", Domain = "domain", Speed = ProxySpeed.Slow },
+        bestProxy,
+        new Proxy {Country = "uk", Domain = "domain", Speed = ProxySpeed.NotResponding },
+        new Proxy {Country = "uk", Domain = "domain", Speed = ProxySpeed.Fast },
+      };
+
+      PirateProxyPicker picker = new PirateProxyPicker(locationPrefs, new StubLogger());
+
+      Proxy chosenProxy = picker.BestProxy(proxies);
+
+      Assert.AreEqual(bestProxy, chosenProxy);
+    }
+
+    [Test]
+    public void TestBestProxyLocationFirstChoice()
+    {
+      List<string> locationPrefs = new List<string> { "uk", "us", "sd" };
+      Proxy bestProxy = new Proxy { Country = "uk", Domain = "domain", Speed = ProxySpeed.Slow };
+
+      List<Proxy> proxies = new List<Proxy>
+      {
+        new Proxy {Country = "us", Domain = "domain", Speed = ProxySpeed.Slow },
+        bestProxy,
+        new Proxy {Country = "sd", Domain = "domain", Speed = ProxySpeed.Slow },
+        new Proxy {Country = "es", Domain = "domain", Speed = ProxySpeed.Slow },
+      };
+
+      PirateProxyPicker picker = new PirateProxyPicker(locationPrefs, new StubLogger());
+
+      Proxy chosenProxy = picker.BestProxy(proxies);
+
+      Assert.AreEqual(bestProxy, chosenProxy);
+    }
+
+    [Test]
+    public void TestBestProxyLocationSecondChoice()
+    {
+      List<string> locationPrefs = new List<string> { "uk", "us", "sd" };
+      Proxy bestProxy = new Proxy { Country = "us", Domain = "domain", Speed = ProxySpeed.Slow };
+
+      List<Proxy> proxies = new List<Proxy>
+      {
+        new Proxy {Country = "sd", Domain = "domain", Speed = ProxySpeed.Slow },
+        bestProxy,
+        new Proxy {Country = "it", Domain = "domain", Speed = ProxySpeed.Slow },
+        new Proxy {Country = "es", Domain = "domain", Speed = ProxySpeed.Slow },
+      };
+
+      PirateProxyPicker picker = new PirateProxyPicker(locationPrefs, new StubLogger());
+
+      Proxy chosenProxy = picker.BestProxy(proxies);
+
+      Assert.AreEqual(bestProxy, chosenProxy);
+    }
+
+    [Test]
+    public void TestBestProxyLocationPreferLocationOverSpeed()
+    {
+      List<string> locationPrefs = new List<string> { "uk", "us", "sd" };
+      Proxy bestProxy = new Proxy { Country = "uk", Domain = "domain", Speed = ProxySpeed.Slow };
+
+      List<Proxy> proxies = new List<Proxy>
+      {
+        new Proxy {Country = "us", Domain = "domain", Speed = ProxySpeed.VeryFast},
+        bestProxy,
+        new Proxy {Country = "sd", Domain = "domain", Speed = ProxySpeed.Fast },
+        new Proxy {Country = "es", Domain = "domain", Speed = ProxySpeed.Fast },
+      };
+
+      PirateProxyPicker picker = new PirateProxyPicker(locationPrefs, new StubLogger());
+
+      Proxy chosenProxy = picker.BestProxy(proxies);
+
+      Assert.AreEqual(bestProxy, chosenProxy);
+    }
+
+    [Test]
+    public void TestBestProxyLocationPicksSpeedTiebreak()
+    {
+      List<string> locationPrefs = new List<string> { "uk", "us", "sd" };
+      Proxy bestProxy = new Proxy { Country = "uk", Domain = "domain", Speed = ProxySpeed.VeryFast };
+
+      List<Proxy> proxies = new List<Proxy>
+      {
+        new Proxy {Country = "uk", Domain = "domain", Speed = ProxySpeed.Fast },
+        bestProxy,
+        new Proxy {Country = "us", Domain = "domain", Speed = ProxySpeed.VeryFast },
+        new Proxy {Country = "sd", Domain = "domain", Speed = ProxySpeed.VeryFast },
+      };
+
+      PirateProxyPicker picker = new PirateProxyPicker(locationPrefs, new StubLogger());
+
+      Proxy chosenProxy = picker.BestProxy(proxies);
+
+      Assert.AreEqual(bestProxy, chosenProxy);
+    }
+
+    [Test]
+    public void TestBestProxyIgnoresBlacklistedDomains()
+    {
+      List<string> locationPrefs = new List<string> { "uk", "us", "sd" };
+      Proxy bestProxy = new Proxy { Country = "us", Domain = "domain1", Speed = ProxySpeed.Fast };
+      Proxy blackListProxy = new Proxy { Country = "uk", Domain = "domain2", Speed = ProxySpeed.VeryFast };
+
+      List<Proxy> proxies = new List<Proxy>
+      {
+        blackListProxy,
+        bestProxy,
+        new Proxy {Country = "us", Domain = "domain3", Speed = ProxySpeed.Slow },
+        new Proxy {Country = "sd", Domain = "domain4", Speed = ProxySpeed.VeryFast },
+      };
+
+      PirateProxyPicker picker = new PirateProxyPicker(locationPrefs, new StubLogger());
+
+      Proxy chosenProxy = picker.BestProxy(proxies);
+      Assert.AreEqual(blackListProxy, chosenProxy);
+
+      picker.BlacklistDomain("domain2");
+
+      chosenProxy = picker.BestProxy(proxies);
+      Assert.AreEqual(bestProxy, chosenProxy);
+    }
+
+    [Test]
+    public void TestBestProxyReturnsNullWhenAllDomainsBlackListed()
+    {
+      List<string> locationPrefs = new List<string> { "uk", "us", "sd" };
+
+      List<Proxy> proxies = new List<Proxy>
+      {
+        new Proxy {Country = "uk", Domain = "domain", Speed = ProxySpeed.Fast},
+        new Proxy {Country = "it", Domain = "domain", Speed = ProxySpeed.NotResponding },
+        new Proxy {Country = "us", Domain = "domain", Speed = ProxySpeed.Slow },
+        new Proxy {Country = "sd", Domain = "domain", Speed = ProxySpeed.VeryFast },
+      };
+
+      PirateProxyPicker picker = new PirateProxyPicker(locationPrefs, new StubLogger());
+
+      picker.BlacklistDomain("domain");
+
+      Proxy chosenProxy = picker.BestProxy(proxies);
+      Assert.IsNull(chosenProxy);
+    }
+
+  }
+}
