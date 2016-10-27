b5a78722a6b16091c7cbf308ddc984ac21c1b29e
Thosedarnmonkeys <thosedarnmonkeys@googlemail.com>
2016-10-27 18:05:13 +0100

    Added logging to MagnetsInSearchTester

--- PirateAPI/ProxyInfoGatherers/MagnetsInSearchTester.cs
+++ PirateAPI/ProxyInfoGatherers/MagnetsInSearchTester.cs
@@ -30,11 +30,18 @@ namespace PirateAPI.ProxyInfoGatherers
     public bool? DomainHasMagnetsInSearch(string domain)
     {
       if (string.IsNullOrWhiteSpace(domain))
+      {
+        logger.LogError("MagnetsInSearchTester.DomainHasMagnetsInSearch was passed a null or empty domain string");
         return null;
+      }
 
-      string top100Page = webClient.DownloadString(domain + top100VideosPath);
+      string top100Url = domain + top100VideosPath;
+      string top100Page = webClient.DownloadString(top100Url);
       if (string.IsNullOrWhiteSpace(top100Page))
+      {
+        logger.LogError($"Domain {domain} didn't respond for url {top100Url}");
         return null;
+      }
 
       string magnetLinkPattern = @"href=""magnet:\?.*?""";
       Regex regex = new Regex(magnetLinkPattern);
